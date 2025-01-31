e59c63befcdbe0b4b7ec4432b5fe9a272a5d2037
anjelica <144828225+anjelica0224@users.noreply.github.com>
2025-01-31 14:30:28 +0530

    feat: initial commit

---
+++ main.py
@@ -0,0 +1,9 @@
+from fastapi import FastAPI
+
+# creating a server with python FastAPI
+app = FastAPI()
+
+# hello world endpoint
+@app.get("/")
+def read_root(): # function that is binded with the endpoint
+    return {"Hello": "World"}
