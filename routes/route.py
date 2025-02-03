b52178b35a028b978f475277067dc4feb569306c
anjelica <144828225+anjelica0224@users.noreply.github.com>
2025-02-03 20:31:35 +0530

    fix: template works

--- routes/route.py
+++ routes/route.py
@@ -13,10 +13,19 @@ db = get_database()
 @router.get("/", response_class=HTMLResponse)
 async def read_item(request: Request):
     documents = db.scores.find({})
+    new_doc = []
     for doc in documents:
         print(doc["_id"])
+        new_doc.append({
+            "id" : doc['_id'],
+            "name" : doc['name'],
+            "time_taken" : doc["time_taken"],
+            "emojis_used" : doc["emojis_used"]
+        })
+    print(new_doc)    
     return templates.TemplateResponse(
-        request=request, name="index.html"
+        request=request, name="index.html", context={"new_doc": new_doc}
     )
 
 # @router.post("/submit-score", response_class = HTMLResponse)
+
