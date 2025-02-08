32f95fd791c38f2183f5ad5e56876ad654585ac9
anjelica <144828225+anjelica0224@users.noreply.github.com>
2025-02-09 03:11:27 +0530

    chore: remove static template

--- index.py
+++ index.py
@@ -1,11 +1,11 @@
 from fastapi import FastAPI
 # from routes.route import router
 from routes.mvlist_route import router
-from fastapi.staticfiles import StaticFiles
+# from fastapi.staticfiles import StaticFiles
 
 
 app = FastAPI()
 app.include_router(router, prefix="/api")
-app.mount("/static", StaticFiles(directory="static"), name="static")
+# app.mount("/static", StaticFiles(directory="static"), name="static")
 
 
